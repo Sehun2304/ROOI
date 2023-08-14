f0bdd774dc711ec7111e2eaa38012b2b1ad4871d
notitle12 <notitle1215@gmail.com>
2023-08-14 09:27:01 +0900

    댓글　삭제　아이디좀　가져와주세요

--- src/main/java/com/rooi/rooi/repository/CommentRepository.java
+++ src/main/java/com/rooi/rooi/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.rooi.rooi.repository;
 import com.rooi.rooi.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByCardId(Long cardId);
+
 }
